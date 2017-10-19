2e8a182c7955eb133e6ba242089e25e88b836374
Joe Liotta <joe.liotta00@gmail.com>
2017-10-19 12:24:30 -0700

    Add template to root Vue instance and play around with life cycle hooks

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -7,16 +7,19 @@ Vue.use(MenuItem);
 
 module.exports = Vue.component('main-nav', {
 	template: `
-	<el-menu theme="light" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">Gratitude</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">Teams</template>
-    <el-menu-item index="2-1">item one</el-menu-item>
-    <el-menu-item index="2-2">item two</el-menu-item>
-    <el-menu-item index="2-3">item three</el-menu-item>
-  </el-submenu>
-  <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">Home</a></el-menu-item>
-</el-menu>`
+		<el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+		  <el-menu-item index="1">Gratitude</el-menu-item>
+		  <el-submenu index="2">
+		    <template slot="title">Teams</template>
+		    <el-menu-item index="2-1">Team 1</el-menu-item>
+		    <el-menu-item index="2-2">Team 2</el-menu-item>
+		    <el-menu-item index="2-3">Team 3</el-menu-item>
+		  </el-submenu>
+		  <el-menu-item index="3"><a href="#" target="_blank">Home</a></el-menu-item>
+		</el-menu>`,
+	created: function() {
+		console.log('CREATED MAIN NAV')
+	}
 });
 
 
--- src/main.js
+++ src/main.js
@@ -5,5 +5,14 @@ var vm = new Vue({
   el: '#root',
   data: {
     message: 'Hello Vue!'
+  },
+  template: `
+  <div>
+	  <main-nav></main-nav>
+	  <h3> {{ this.message }} </h3>
+  </div>
+  `,
+  beforeMount: function() {
+  	console.log('BEFORE MOUNT');
   }
 });
